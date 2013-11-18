4b8b8e111a087223c19c85bf2dcf84ebfe7cf8ea
Niclas Olsson <niclas@cobton.se>
2013-11-18 16:48:37 +0100

    Initialt ramverk 20131118

---
+++ src/scalesweden/ScaleClasses.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package scalesweden;
+
+/**
+ *
+ * @author niclas
+ */
+public interface ScaleClasses {
+    
+    String[] ListOfClasses = {"F4C", "F4H", "Klubbskala", "Populärskala"};
+    
+}
